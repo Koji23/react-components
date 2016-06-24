df778f9898aabc3be4be70a780e0529ef4556119
Jordan Chong <jordanchong23@gmail.com>
2016-06-24 12:06:11 -0700

    Implement static components.
    Create GroceryList component
    Render to page
    Create GroceryItem Component
    Create App Component
    Implement props for dynamic list items
    Implement onClick listener for GroceryItem components

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // TODO
+
+//note this array must be declared before invoking 'items'  duh!
+var groceryItems = ['milk', 'eggs', 'chicken', 'babel bites'];
+
+//invoke with groceryItems here
+var App = () => {
+  return (
+    <div>
+      <h2>My Grocery List</h2> 
+      <GroceryList items={ groceryItems } />
+    </div>
+    );
+};
+
+//using {} and return here just for fun
+var GroceryList = (props) => {
+  return ( 
+    <ul>
+      <GroceryItem item={ props.items[0] } /> 
+      <GroceryItem item={ props.items[1] } />
+      <GroceryItem item={ props.items[2] } />
+      <GroceryItem item={ props.items[3] } />
+    </ul>
+  );
+};
+     // {props.items.map(food => <GroceryItem item = { food }/> ) }
+
+//note the paraethesis implies that everything inside will be returned as one line
+// !!! note that listeners for events must be on the html elements, not the components
+// the onClick attribute below is an HTML attribute
+var GroceryItem = (props) => {
+  var onListItemClick = (event) => {
+    console.log("Click heard on", props.item, "!");
+  };
+  return <li onClick={onListItemClick}>{props.item}</li>
+};
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
